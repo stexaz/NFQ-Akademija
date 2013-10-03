08ffe1ca214d2b900142354dd5996f4bdac995c2
Vagrant User <vagrant@nfqakademija.dev>
2013-10-03 00:43:54 +0000

    Atnaujintas kodas

--- index.php
+++ index.php
@@ -4,28 +4,37 @@
  * Date: 10/1/13
  */
 
-require_once 'Upload.php';
-require_once 'Display.php';
+function __autoload($name)
+{
+    if (file_exists($name.'.php')) include $name.'.php';
+}
 
 $upload = new Upload;
 $display = new Display;
 
 $display->image = '';
+$display->message_type = 'ok';
 
 if ($upload->isUploading()) {
 $display->message = 'Norite įkelti kitą paveikslėlį?';
-
-    if ($upload->done()) $display->image = <<<IMG
+    if ($upload->done()) {
+        $url = $upload->getUri();
+        $display->image = <<<IMG
 <div class="img">
-    <img src="" alt="*"/>
+    <img src="$url" alt="*"/>
 </div>
 IMG;
-
+    }
+    if ($upload->error()) {
+        $display->message = $upload->error();
+        $display->message_type = 'error';
+    }
 } else {
 $display->message = 'Pasirinkite paveikslėlį';
 }
 
-$display->file_types = implode(', ', array('jpg', 'png', 'gif'));
+$display->file_types = implode(', ', Image::formats());
+$display->accept = implode(',', Image::formats(true));
 $display->max_size = ini_get('upload_max_filesize').'b';
 
 $display->render('upload_form.phtml');
