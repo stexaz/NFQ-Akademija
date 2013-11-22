90ba055a1218e152e4533b55094cbf80c1607655
Andrius Petrauskis <stexaz@gmail.com>
2013-11-22 22:50:27 +0200

    The jquery UI plugin.

---
+++ public/js/jquery.validator.js
@@ -0,0 +1,36 @@
+(function($){
+
+    function bind(fn,obj) {
+        return function() {
+            fn.apply(obj,arguments);
+        };
+    }
+
+    function ValidateUser() {};
+
+    ValidateUser.prototype._create = function()
+    {
+        this._on(this.element,{
+            "change": "onChange"
+        });
+    };
+
+    ValidateUser.prototype.onChange = function(event) {
+        this.checkUser();
+    };
+
+    ValidateUser.prototype.checkUser = function()
+    {
+        $.post('/user_check', {username: this.element.attr('value')}, bind(this._handleResponse, this),'json');
+    };
+
+    ValidateUser.prototype._handleResponse = function(data)
+    {
+        if (data.status == 'registered') {
+            this.element.addClass('invalid');
+        }
+    };
+
+    $.widget('validateUser', ValidateUser.prototype);
+
+})(jQuery);
